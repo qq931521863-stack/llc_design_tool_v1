8835c5b9cbc7a14daffcd461f25791f63764cbda
yangshuai <yangshuai2022@gmail.com>
2026-09-15 13:23:31 +0800

    feat: export FRA advanced GUI dialogs

--- power_control_tools/gui/__init__.py
+++ power_control_tools/gui/__init__.py
@@ -1,4 +1,11 @@
 from .main_window import ControlToolsMainWindow
 from .fra_loop_designer import FRALoopDesignerWindow
+from .fra_advanced import FRAAutoDesignDialog, FRAModelFitDialog, install_advanced_fra_actions
 
-__all__ = ["ControlToolsMainWindow", "FRALoopDesignerWindow"]
+__all__ = [
+    "ControlToolsMainWindow",
+    "FRALoopDesignerWindow",
+    "FRAAutoDesignDialog",
+    "FRAModelFitDialog",
+    "install_advanced_fra_actions",
+]
