3db7b4525350fbfe606a96e355dc28ddc763345d
yangshuai <yangshuai2022@gmail.com>
2026-09-16 15:16:02 +0800

    feat(pfc): export TTPL shared-ngspice APIs

--- power_sim/spice/__init__.py
+++ power_sim/spice/__init__.py
@@ -24,6 +24,15 @@ from .llc import LLCSpiceConfig, build_ideal_llc_circuit, default_llc_transient_
 from .gate import FrequencySegment, FrequencyTimeline, LLCFullBridgeGateScheduler
 from .sync import SyncEvent, LLCCoSimulationSynchronizer
 from .closed_loop import NgSpiceClosedLoopConfig, NgSpiceClosedLoopResult, run_llc_shared_closed_loop
+from .pfc import DutySegment, DutyTimeline, TTPLGateScheduler, TTPLSpiceConfig, build_ttpl_pfc_circuit
+from .pfc_closed_loop import (
+    TTPLClosedLoopScenario,
+    TTPLControlSample,
+    TTPLSharedNgSpiceConfig,
+    TTPLSharedNgSpiceMetrics,
+    TTPLSharedNgSpiceResult,
+    run_ttpl_shared_closed_loop,
+)
 from .waveforms import ngspice_closed_loop_waveform_bundle
 
 __all__ = [
@@ -53,5 +62,16 @@ __all__ = [
     "NgSpiceClosedLoopConfig",
     "NgSpiceClosedLoopResult",
     "run_llc_shared_closed_loop",
+    "DutySegment",
+    "DutyTimeline",
+    "TTPLGateScheduler",
+    "TTPLSpiceConfig",
+    "build_ttpl_pfc_circuit",
+    "TTPLClosedLoopScenario",
+    "TTPLControlSample",
+    "TTPLSharedNgSpiceConfig",
+    "TTPLSharedNgSpiceMetrics",
+    "TTPLSharedNgSpiceResult",
+    "run_ttpl_shared_closed_loop",
     "ngspice_closed_loop_waveform_bundle",
 ]
